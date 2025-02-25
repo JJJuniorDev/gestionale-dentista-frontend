4e0b0ff9c5b5b42b9b5372383549b093dbda329b
lorenzodetoma <lorenzo.detoma@eng.it>
2025-02-25 12:47:15 +0100

    Fix: aggiunto api al path di sicurezza

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://sicurezza.fly.dev', // URL del backend in produzione
+  apiUrl: 'https://sicurezza.fly.dev/api', // URL del backend in produzione
   appuntamentoMicroserviceUrl: 'https://appuntamenti.fly.dev/',
   pazientiMicroserviceUrl: 'https://microservizio-pazienti.fly.dev/',
 };
