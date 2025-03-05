764101aaf7bcfe5dad17e9af00880965397fb348
lorenzodetoma <lorenzo.detoma@eng.it>
2025-03-05 23:41:31 +0100

    tolti routing versione 1.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   // appuntamentoMicroserviceUrl: 'https://appuntamenti.fly.dev/api',
   // pazientiMicroserviceUrl: 'https://microservizio-pazienti.fly.dev/api',
   apiUrl: 'https://app-icy-hill-3624.fly.dev/api',
-  appuntamentoMicroserviceUrl: 'https://app-icy-hill-3624.fly.dev/api',
-  pazientiMicroserviceUrl: 'https://app-icy-hill-3624.fly.dev/api',
+  appuntamentoMicroserviceUrl: 'https://app-icy-hill-3624.fly.dev/api/appuntamenti',
+  pazientiMicroserviceUrl: 'https://app-icy-hill-3624.fly.dev/api/pazienti',
 };
